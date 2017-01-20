9172a02e0787fdb0486184e1eb0ba84bbf7a625c
Dennis Parker <dennis.parker@gmail.com>
2017-01-20 15:55:47 -0600

    Last commit did the thread local storage wrong. Also added exception print for errors in response processing. Added ignore switch for 304 codes to be smarter about no change on static files such as images

--- django_logging_dlp/middleware.py
+++ django_logging_dlp/middleware.py
@@ -1,35 +1,41 @@
 import datetime
 import threading
+import traceback
 from django.db import connections
 from . import log
 from . import settings
 from .log_object import LogObject, ErrorLogObject, SqlLogObject
 
 
+thread_data = threading.local()
 class DjangoLoggingMiddleware(object):
     def process_exception(self, request, exception):
         error = ErrorLogObject(request, exception)
         log.error(error)
 
     def process_request(self, request):
-        self.tdata = threading.local()
-        self.tdata.start = datetime.datetime.now()
+        thread_data.start = datetime.datetime.now()
 
     def process_response(self, request, response):
-        done = datetime.datetime.now()
-        delta = (done - self.tdata.start).total_seconds()
-        for connection in connections.all():
-            self.log_connection_queries(connection)
+        try:
+            done = datetime.datetime.now()
+            delta = (done - thread_data.start).total_seconds()
+            for connection in connections.all():
+                self.log_connection_queries(connection)
 
-        if request.path_info.startswith(tuple(settings.IGNORED_PATHS)):
-            return response
+            if request.path_info.startswith(tuple(settings.IGNORED_PATHS)):
+                return response
 
-        if response.status_code == 500:
-            return response
-        elif 400 <= response.status_code < 500:
-            log.warning(LogObject(request, response, delta))
-        else:
-            log.info(LogObject(request, response, delta))
+            if response.status_code == 500:
+                return response
+            if response.status_code == 304:
+                return response
+            elif 400 <= response.status_code < 500:
+                log.warning(LogObject(request, response, delta))
+            else:
+                log.info(LogObject(request, response, delta))
+        except Exception as e:
+            traceback.print_exc()
         return response
 
     def log_connection_queries(self, connection):
